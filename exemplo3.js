dbb6f10d7db9f7922d46185e7fccec4c3e27dac4
eraldovicente <eraldovicente2@gmail.com>
2019-07-29 21:37:29 -0300

    retificação

--- exemplo3.js
+++ exemplo3.js
@@ -1,6 +1,6 @@
 /* O operador bitwise atuado como uma flag de validação.
 *  Se a função receber  segundo parâmetro será então atribuido,
-*  senão o valor "1" será definido.
+*  senão o valor "1" será atribuido.
 */ 
 function somar(x, y) {
     y = y | 1;            // O "pipe" é o operador bitwise or
