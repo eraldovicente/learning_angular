15b82c629dd91693115e108ec144d7246b0109c4
eraldovicente <eraldovicente2@gmail.com>
2019-07-29 22:10:00 -0300

    exemplo8.js

---
+++ exemplo8.js
@@ -0,0 +1,10 @@
+// Map
+
+// O callback da função map percorre item por item da variável
+var arrayNumeros = [2, 3, 4, 5, 6];
+
+var novoValor = arrayNumeros.map(function(num) {
+    return num * 2;
+});
+
+console.log(novoValor);
