9a0dab5ce210e0b0d20d906c148acc5f0a5e34b7
eraldovicente <eraldovicente2@gmail.com>
2019-07-30 20:02:23 -0300

    tipagem

--- exemplo13.ts
+++ exemplo13.ts
@@ -5,3 +5,16 @@ let nome = "Eraldo Vicente";
 function dizOla(nome: string) {
     console.log("Olá " + nome);   // Saída será: Olá Eraldo Vicente
 }
+
+dizOla(nome);
+
+// Caso eu passe um argumento de tipo diferente do
+// parâmetro o compilador TypeScript emite um erro
+
+let nome1 = 15;
+
+function dizOla2(nome: string) {
+    console.log("Olá " + nome); // Argument of type 'number' is not assignable to parameter of type 'string'
+}
+
+dizOla2(nome1);
