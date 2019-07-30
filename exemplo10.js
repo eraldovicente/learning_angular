a042ebe3a7b17066057e7ff6ec2e21fa6d5f4888
eraldovicente <eraldovicente2@gmail.com>
2019-07-30 19:23:27 -0300

    exemplo10

---
+++ exemplo10.js
@@ -0,0 +1,10 @@
+// Filter 
+nomes = ['jorge', 'carlos', 'roberto', 'lucas']; 
+
+novoNome = nomes.filter(function(nome) { 
+// Com o include não precisamos fazer uma validação ele 
+// retorna true ou false automaticamente 
+return nome.includes('a'); 
+}); 
+
+console.log(novoNome); // Saída será "carlos" e "lucas"
