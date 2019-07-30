981bf28a742cce81cd1e2e6e171f6bb5e62cb9d4
eraldovicente <eraldovicente2@gmail.com>
2019-07-30 19:47:20 -0300

    comentário

---
+++ exemplo12.ts
@@ -0,0 +1,11 @@
+// Tema - tipagem de variável
+
+let meuNome = "Eraldo Vicente";
+
+console.log(meuNome);  
+
+// Em um arquivo TypeScript após uma variável ser declarada 
+// com "let" não é possivel declarar outra variável com o mesmo nome
+var meuNome = 15;
+
+console.log(meuNome); 
