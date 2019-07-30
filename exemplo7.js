1f7058c0cb3ff4e42a81544465252db1914cc8bc
eraldovicente <eraldovicente2@gmail.com>
2019-07-29 22:03:53 -0300

    exemplo7.js

---
+++ exemplo7.js
@@ -0,0 +1,9 @@
+// Destructuring
+
+// Uso de destructuring com objetos
+meuObjeto = {livro:'Angular 2', autor:'thiago guedes'};
+
+const {livro, autor} = meuObjeto;
+
+console.log(livro); // Saída será "Angular 2"
+console.log(autor); // Saída será "thiago guedes"
