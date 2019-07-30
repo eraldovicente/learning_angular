ef3f49553c495a5d3e30df2dfe3be18364647704
eraldovicente <eraldovicente2@gmail.com>
2019-07-30 19:36:33 -0300

    soma de valores

--- exemplo11.js
+++ exemplo11.js
@@ -5,3 +5,9 @@ var nomes = ['jorge', 'carlos', 'roberto', 'lucas'];
 var novoNome = nomes.reduce((acumulado, nome) => acumulado + '-' + nome);
 
 console.log(novoNome); // Saída será: jorge-carlos-roberto-lucas
+
+var valores = [10, 20, 30, 40, 50];
+
+var novoValor = valores.reduce((acumulado, corrente) => acumulado + corrente);
+
+console.log(novoValor); // saída será: 150
