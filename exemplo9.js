d80b5600ba39427c223cf9dc5cda38d55cedfcfa
eraldovicente <eraldovicente2@gmail.com>
2019-07-29 22:15:35 -0300

    exemplo9.js

---
+++ exemplo9.js
@@ -0,0 +1,7 @@
+// Uso de Arrow Functions com o map
+
+var arrayNumeros = [2, 3, 4, 5, 6];
+
+var novoValor = arrayNumeros.map((num) => num * 2);
+
+console.log(novoValor); // Saída será [4, 6, 8, 10, 12]
